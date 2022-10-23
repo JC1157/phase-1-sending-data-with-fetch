47b4a765e5562dcdb3711a75331525e195ec8542
Juan Baez <juan.baez2093@gmail.com>
2022-10-23 15:38:28 -0400

    commit tree bd45b433ef4a365d2adfd43ec7176548f1e05f33

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+// Headers for 'Content-Type' and 'Accept', both set to 'application/json
+
+function submitData () {
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept': 'application/json'
+        }, 
+        body: JSON.stringify({
+            "name" : 'Steve',
+            'email' : 'steve@steve.com'
+        })
+    }) 
+    .then(response => response.json() )
+    .then(returnData => returnData.id)
+    .catch(error => error.message)
+    
+       
+       
+       
+       
+       
+}
+submitData ()
